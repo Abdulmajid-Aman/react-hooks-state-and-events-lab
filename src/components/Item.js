542997a3a889fbfc7961d25b33bc9ca1a6852177
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-08-02 23:12:32 +0300

    State and events lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState} from "react";
 
 function Item({ name, category }) {
+  const [buy, setBuy] = useState(false)
+  function handleClick() {
+    console.log("Button was clicked!!!");
+    setBuy((buy) => !buy)
+    console.log(buy);
+    
+  }
   return (
-    <li className="">
+    <li className={buy ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>{buy ? "Remove to cart" : "Add to cart"}</button>
     </li>
   );
 }
